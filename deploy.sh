522e720c1eee6c72dfcc9cd5d1d9d2f58159f24c
imroc <roc@imroc.io>
2018-01-13 11:53:59 +0800

    remove deploy.sh in gh-pages

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ gitbook build .
 git checkout gh-pages
 rm -rf `ls | egrep -v _book`
 mv _book/* ./
-rm -rf _book
+rm -rf _book deploy.sh
 msg="rebuilding site `date`"
 git add .
 git commit -m "$msg"
