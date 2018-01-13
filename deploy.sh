d8ec21d5dc35ef18ea618d4db7eada50ca3810bc
imroc <roc@imroc.io>
2018-01-13 11:39:17 +0800

    rebuilding site Sat Jan 13 11:39:17 CST 2018

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -eux
+gitbook build .
+git checkout gh-pages
+rm -rf `ls | egrep -v _book`
+mv _book/* ./
+rm -rf _book
+msg="rebuilding site `date`"
+git add .
+git commit -m "$msg"
+git push
+git checkout master
